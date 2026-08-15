9ef96f28215b9f7a6e11e79ff3ae7ac6233bab89
hamedgholamzade-art <hamedgholamzade@gmail.com>
2026-08-16 02:46:42 +0330

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const APP_CONFIG = {
   trackApi: "https://hamedgholamzade.app.n8n.cloud/webhook/track-status",
   listApi: "https://hamedgholamzade.app.n8n.cloud/webhook/admin-list",
   updateApi: "https://hamedgholamzade.app.n8n.cloud/webhook/admin-update",
-  smsApi: "",
+  recoverApi: "https://hamedgholamzade.app.n8n.cloud/webhook/recover-code",
   maxFiles: 5,
   maxFileSizeMB: 10
 };
